52b4c4c687a19c9d555f91c677d25ad7bc9a414a
Rasyid Rafi <muhammand.rafi25@gmail.com>
2021-03-08 01:44:26 +0700

    Trying to deploy to github

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # R45 Music
+
 Web Based Music Player that can play music from Youtube and Joox  
-Menggunakan NodeJs dan beberapa framework untuk memutar musik dari youtube dan joox  
+Menggunakan NodeJs dan beberapa framework untuk memutar musik dari youtube dan joox
 
 ## Disclaimer
-*Copyright Disclaimer under section 107 of the Copyright Act of 1976, allowance is made for "fair use" for purposes such as criticism, comment, news reporting, teaching, scholarship, education and research. Fair use is a use permitted by copyright statute that might otherwise be infringing.*    
+
+_Copyright Disclaimer under section 107 of the Copyright Act of 1976, allowance is made for "fair use" for purposes such as criticism, comment, news reporting, teaching, scholarship, education and research. Fair use is a use permitted by copyright statute that might otherwise be infringing._
 
 Downloading content from YouTube is against the YouTube Policy. I made this project just for **EDUCATION** and **PERSONAL USE** only.
 
 ## Installation
+
 Use the package manager NPM to install R45 Music.
 
 ```javascript
@@ -20,9 +23,23 @@ npm install
 npm start
 ```
 
+## Configuration
+
+Open config.js to configure.
+
+## To DO
+
+- [ ] Re Design the Web
+- [ ] Simplify Code
+- [ ] SoundCloud
+- [ ] Next and Previus Button
+- [ ] Auto play other music when finish
+
 ## Contributing
+
 Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.  
 **Urgent.** Im looking for someone who can design the web (FRONT END) for R45 Music
 
 ## License
+
 [MIT](https://choosealicense.com/licenses/mit/)
--- index.js
+++ index.js
@@ -1,133 +1,135 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var axios = require('axios');
-var ytdl = require('ytdl-core');
-const yts = require('yt-search')
-const ytlist = require('ytpl');
+var express = require("express");
+var bodyParser = require("body-parser");
+var axios = require("axios");
+var ytdl = require("ytdl-core");
+const yts = require("yt-search");
+const ytlist = require("ytpl");
 
-var app = express()
+const config = require("./config");
+
+var app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extend: true }));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+app.set("views", __dirname + "/views");
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 const download = (url, path, callback) => {
-    request.head(url, (err, res, body) => {
-        request(url)
-            .pipe(fs.createWriteStream(path))
-            .on('close', callback)
-    })
-}
+  request.head(url, (err, res, body) => {
+    request(url).pipe(fs.createWriteStream(path)).on("close", callback);
+  });
+};
 
-app.get('/', async (req, res) => {
-    res.render('lobby.html')
+app.get("/", async (req, res) => {
+  res.render("lobby.html");
 });
 
-app.get('/yt', async (req, res) => {
-    if (req.query.url) {
-        const hasil = await ytdl.getInfo(req.query.url);
-        const jsonnya = hasil.formats;
-        let direct = ""
-        let long = hasil.videoDetails.thumbnails.length - 1
-        let thumb = hasil.videoDetails.thumbnails[long].url
-        let title = hasil.videoDetails.title
-        for (let i = 0; i < jsonnya.length; i++) {
-            if (jsonnya[i].hasVideo == false) {
-                direct = jsonnya[i].url
-            }
+app.get("/yt", async (req, res) => {
+  if (req.query.url) {
+    const hasil = await ytdl.getInfo(req.query.url);
+    const jsonnya = hasil.formats;
+    let direct = "";
+    let long = hasil.videoDetails.thumbnails.length - 1;
+    let thumb = hasil.videoDetails.thumbnails[long].url;
+    let title = hasil.videoDetails.title;
+    for (let i = 0; i < jsonnya.length; i++) {
+      if (jsonnya[i].hasVideo == false) {
+        direct = jsonnya[i].url;
+      }
+    }
+    res.render("index.html", { direct: direct, title: title, thumb: thumb });
+  } else if (req.query.judul) {
+    yts(req.query.judul).then(async (result) => {
+      let url = await result.all[0].url;
+      const hasil = await ytdl.getInfo(url);
+      const jsonnya = hasil.formats;
+      let direct = "";
+      let long = hasil.videoDetails.thumbnails.length - 1;
+      let thumb = hasil.videoDetails.thumbnails[long].url;
+      let title = hasil.videoDetails.title;
+      for (let i = 0; i < jsonnya.length; i++) {
+        if (jsonnya[i].hasVideo == false) {
+          direct = jsonnya[i].url;
         }
-        res.render('index.html', { direct: direct, title: title, thumb: thumb });
-    }
-    else if (req.query.judul) {
-        yts(req.query.judul)
-            .then(async (result) => {
-                let url = await result.all[0].url
-                const hasil = await ytdl.getInfo(url);
-                const jsonnya = hasil.formats;
-                let direct = ""
-                let long = hasil.videoDetails.thumbnails.length - 1
-                let thumb = hasil.videoDetails.thumbnails[long].url
-                let title = hasil.videoDetails.title
-                for (let i = 0; i < jsonnya.length; i++) {
-                    if (jsonnya[i].hasVideo == false) {
-                        direct = jsonnya[i].url
-                    }
-                }
-                res.render('index.html', { direct: direct, title: title, thumb: thumb });
-            })
-    }
-    else if (req.query.playlist) {
-        let awal = req.query.playlist
-        let link = awal.split('=')[1]
-        ytlist(link).then(async (oi) => {
-            let listRandom = getRandomInt(oi.items.length)
-            let filterUrl = oi.items[listRandom].url.split('&')[0]
+      }
+      res.render("index.html", { direct: direct, title: title, thumb: thumb });
+    });
+  } else if (req.query.playlist) {
+    let awal = req.query.playlist;
+    let link = awal.split("=")[1];
+    ytlist(link).then(async (oi) => {
+      let listRandom = getRandomInt(oi.items.length);
+      let filterUrl = oi.items[listRandom].url.split("&")[0];
 
-            const hasil = await ytdl.getInfo(filterUrl);
-            const jsonnya = hasil.formats;
-            let direct = ""
-            let long = hasil.videoDetails.thumbnails.length - 1
-            let thumb = hasil.videoDetails.thumbnails[long].url
-            let title = hasil.videoDetails.title
-            for (let i = 0; i < jsonnya.length; i++) {
-                if (jsonnya[i].hasVideo == false) {
-                    direct = jsonnya[i].url
-                }
-            }
-            res.render('index.html', { direct: direct, title: title, thumb: thumb });
-        })
-    }
-    else {
-        let custom = ""
-        let list = [
-            "PLeCdlPO-XhWFzEVynMsmosfdRsIZXhZi0",
-            "PLhsz9CILh357zA1yMT-K5T9ZTNEU6Fl6n",
-            custom
-        ]
+      const hasil = await ytdl.getInfo(filterUrl);
+      const jsonnya = hasil.formats;
+      let direct = "";
+      let long = hasil.videoDetails.thumbnails.length - 1;
+      let thumb = hasil.videoDetails.thumbnails[long].url;
+      let title = hasil.videoDetails.title;
+      for (let i = 0; i < jsonnya.length; i++) {
+        if (jsonnya[i].hasVideo == false) {
+          direct = jsonnya[i].url;
+        }
+      }
+      res.render("index.html", { direct: direct, title: title, thumb: thumb });
+    });
+  } else {
+    let custom = "";
+    let list = [
+      "PLeCdlPO-XhWFzEVynMsmosfdRsIZXhZi0",
+      "PLhsz9CILh357zA1yMT-K5T9ZTNEU6Fl6n",
+      custom,
+      //Put our youtube playlist here
+    ];
 
-        let url = list[getRandomInt(2)]
-        ytlist(url).then(async (oi) => {
-            let listRandom = getRandomInt(oi.items.length)
-            let filterUrl = oi.items[listRandom].url.split('&')[0]
+    let url = list[getRandomInt(2)];
+    ytlist(url).then(async (oi) => {
+      let listRandom = getRandomInt(oi.items.length);
+      let filterUrl = oi.items[listRandom].url.split("&")[0];
 
-            const hasil = await ytdl.getInfo(filterUrl);
-            const jsonnya = hasil.formats;
-            let direct = ""
-            let long = hasil.videoDetails.thumbnails.length - 1
-            let thumb = hasil.videoDetails.thumbnails[long].url
-            let title = hasil.videoDetails.title
-            for (let i = 0; i < jsonnya.length; i++) {
-                if (jsonnya[i].hasVideo == false) {
-                    direct = jsonnya[i].url
-                }
-            }
-            res.render('index.html', { direct: direct, title: title, thumb: thumb });
-        })
-    }
-})
+      const hasil = await ytdl.getInfo(filterUrl);
+      const jsonnya = hasil.formats;
+      let direct = "";
+      let long = hasil.videoDetails.thumbnails.length - 1;
+      let thumb = hasil.videoDetails.thumbnails[long].url;
+      let title = hasil.videoDetails.title;
+      for (let i = 0; i < jsonnya.length; i++) {
+        if (jsonnya[i].hasVideo == false) {
+          direct = jsonnya[i].url;
+        }
+      }
+      res.render("index.html", { direct: direct, title: title, thumb: thumb });
+    });
+  }
+});
 
-app.get('/joox', async (req, res) => {
-    if (req.query.judul) {
-        axios.get(`https://mnazria.herokuapp.com/api/jooxnich?search=${req.query.judul}`)
-            .then(async (hasil) => {
-                let thumb = hasil.data.result.album_url
-                let title = hasil.data.result.msong
-                let direct = hasil.data.result.mp3Url
-                res.render('index.html', { direct: direct, title: title, thumb: thumb });
-            })
-    }
-    else {
-        res.send('Masukkan parameter judul')
-    }
-})
+app.get("/joox", async (req, res) => {
+  if (req.query.judul) {
+    axios
+      .get(
+        `https://mnazria.herokuapp.com/api/jooxnich?search=${req.query.judul}`
+      )
+      .then(async (hasil) => {
+        let thumb = hasil.data.result.album_url;
+        let title = hasil.data.result.msong;
+        let direct = hasil.data.result.mp3Url;
+        res.render("index.html", {
+          direct: direct,
+          title: title,
+          thumb: thumb,
+        });
+      });
+  } else {
+    res.send("Masukkan parameter judul");
+  }
+});
 
-let PORT = process.env.PORT || 8000
+let PORT = config.PORT;
 app.listen(PORT, () => {
-    console.log("Listening on " + PORT);
+  console.log("Listening on " + PORT);
 });
